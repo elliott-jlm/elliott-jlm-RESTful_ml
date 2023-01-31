6d7476c04cf5b398fe4d453322f64beea065846c
Elliott Joliman <72805656+elliott-jlm@users.noreply.github.com>
2023-01-31 17:36:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
-
-"""
-simple python flask application
-"""
-
-##########################################################################
-## Imports
-##########################################################################
-
-import os
-
 from flask import Flask
 from flask import request
-from flask import render_template
-from flask import url_for
 from flask.json import jsonify
 import pickle
 
-
 model = pickle.load(open("trained_model.pkl", "rb"))
-classes = ['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-##########################################################################
-## Routes
-##########################################################################
+classes = ['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 @app.route("/classify", methods=["POST"])
 def classify():
@@ -32,8 +15,6 @@ def classify():
 
     # Convert the data to a numpy array
     pixels = np.array(data["pixels"])
-
-    # Reshape the array to match the model's input shape
     pixels = pixels.reshape(1, -1)
 
     # Use the model to make a prediction
@@ -44,10 +25,6 @@ def classify():
 
     # Return the predicted class as a response
     return {"class": classes[class_index]}
-    
-##########################################################################
-## Main
-##########################################################################
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
