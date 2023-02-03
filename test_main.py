1baa34ddf28a5da7a7f4d838a7281d0077950605
Elliott Joliman <72805656+elliott-jlm@users.noreply.github.com>
2023-02-03 22:44:59 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,28 +1,30 @@
 import unittest
-# from app import app
+from app import app
 import requests
 
 class FlaskTestCase(unittest.TestCase):
+    
+    def crete_app(self):
+        return app
+    
+    def test_classify(self):
+        # Define the URL of the endpoint
+        url = "http://localhost:5000/classify"
 
-    # Define the URL of the endpoint
-    url = "http://localhost:5000/classify"
+        # Define a sample row of pixels using a list comprehension
+        pixels = [0 if i % 2 == 0 else 255 for i in range(28 * 28)]
 
-    # Define a sample row of pixels using a list comprehension
-    pixels = [0 if i % 2 == 0 else 255 for i in range(28 * 28)]
+        # Make a request to the API endpoint
+        response = requests.post(url, json={"pixels": pixels})
 
-    # Make a request to the API endpoint
-    response = requests.post(url, json={"pixels": pixels})
+        # Parse the response as a JSON object
+        result = response.json()
 
-    # Check the status code of the response
-    assert response.status_code == 200
-
-    # Parse the response as a JSON object
-    result = response.json()
-
-    # Check the expected result against the actual result
-    expected_result = 2
-    actual_result = result["class"]
-    assert expected_result == actual_result
+        # Check the expected result against the actual result
+        expected_result = {"class": '0'}
+        
+        assert result == expected_result, f"Expected {expected_result} but got {result}"
+        print("Test passed!")
 
 if __name__ == '__main__':
     unittest.main()
